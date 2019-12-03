a4defca30461196bf7797ab518a3abfad0edc41b
Joely Vernier <55214971+jorovernier@users.noreply.github.com>
2019-12-03 15:24:12 -0700

    Styled header and landing

--- void/src/Components/Landing/Landing.js
+++ void/src/Components/Landing/Landing.js
@@ -1,5 +1,11 @@
 import React from "react";
+import './Landing.scss';
 
 export default function Landing() {
-  return <div>Home Page</div>;
+  return (
+    <div className='home-main'>
+      <h2>IT'S TIME TO ENTER</h2>
+      <h1>THE VOID</h1>
+    </div>
+  );
 }

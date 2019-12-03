cf32a95e4e4b114c97749b2efc4861cddf495087
Hoover <thoover1@uab.edu>
2019-12-03 13:25:49 -0600

    required socket.io in backend

--- void/server/server.js
+++ void/server/server.js
@@ -2,15 +2,33 @@ require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 const massive = require("massive");
+const socket = require("socket.io");
+const uc = require("./controllers/userCtrl");
 
 const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env;
 
 const app = express();
 
+app.use(express.json());
+
+const server = app.listen(SERVER_PORT, () => {
+  console.log(`Server running on port ${SERVER_PORT}`);
+});
+
 // for static server
 // app.use(express.static(__dirname + `../build`));
 
-app.use(express.json());
+// start of sockets
+const io = socket(server);
+// console.log(io);
+io.on("connection", socket => {
+  console.log(socket.id);
+
+  socket.on("SEND_MESSAGE", function(data) {
+    io.emit("RECEIVE_MESSAGE", data);
+  });
+});
+// end of sockets
 
 app.use(
   session({
@@ -40,7 +58,3 @@ app.delete("/auth/logout", uc.logout);
 //   if (req.session.user) return next();
 //   else res.sendStatus(401);
 // });
-
-app.listen(SERVER_PORT, () => {
-  console.log(`Server running on port ${SERVER_PORT}`);
-});
---
+++ void/src/Components/Main/sockets.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function sockets() {
+  return <div>sockets component</div>;
+}

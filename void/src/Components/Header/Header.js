922677362f0c901fa211611b661a9097dffc5bf3
Joely Vernier <joelyvernier@Joelys-MacBook-Air.local>
2019-12-03 15:24:12 -0700

    Styled header and landing

--- void/src/Components/Header/Header.js
+++ void/src/Components/Header/Header.js
@@ -1,16 +1,17 @@
 import React from "react";
 import logo from "../../void_logo.png";
 import {NavLink} from 'react-router-dom';
+import './Header.scss';
 
 export default function Header() {
   return (
-    <div>
-      <img src={logo} className="App-logo" alt="logo" />
-      <span>
-        <NavLink to='/' >Home</NavLink>
-        <NavLink to='/login-register' >Login</NavLink>
-        <NavLink to='/dashboard' >Dashboard</NavLink>
-        <NavLink to='/profile' >Profile</NavLink>
+    <div className='header-main' >
+      <img className='logo' src={logo} alt="logo" />
+      <span className='navbar'>
+        <NavLink className='nav' activeClassName='active' to='/' >Home</NavLink>
+        <NavLink className='nav' activeClassName='active' to='/login-register' >Login</NavLink>
+        <NavLink className='nav' activeClassName='active' to='/dashboard' >Dashboard</NavLink>
+        <NavLink className='nav' activeClassName='active' to='/profile' >Profile</NavLink>
       </span>
     </div>
   );

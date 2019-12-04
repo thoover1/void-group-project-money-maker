3a0a62c184a4d61add63f1dc728bac4a2cd83dab
Isaac Hansen <isaac@Isaacs-MacBook-Pro-2.local>
2019-12-04 09:33:19 -0700

    completed sidebar and changed header

--- void/src/Components/Header/Header.js
+++ void/src/Components/Header/Header.js
@@ -1,17 +1,17 @@
 import React from "react";
 import logo from "../../void_logo.png";
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.scss';
 
 export default function Header() {
   return (
     <div className='header-main' >
-      <img className='logo' src={logo} alt="logo" />
+      <NavLink to='/'>
+        <img className='logo' src={logo} alt="logo" />
+      </NavLink>
       <span className='navbar'>
-        <NavLink className='nav' activeClassName='active' to='/' >Home</NavLink>
-        <NavLink className='nav' activeClassName='active' to='/login-register' >Login</NavLink>
         <NavLink className='nav' activeClassName='active' to='/dashboard' >Dashboard</NavLink>
-        <NavLink className='nav' activeClassName='active' to='/profile' >Profile</NavLink>
+        <NavLink className='nav' activeClassName='active' to='/login-register' >Login</NavLink>
       </span>
     </div>
   );

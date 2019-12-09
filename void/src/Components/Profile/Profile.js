3674ea2c318aee093e8fedc76c6334cc360fffb3
Joely Vernier <55214971+jorovernier@users.noreply.github.com>
2019-12-09 16:22:27 -0700

    fixed logout function and made profile responsive

--- void/src/Components/Profile/Profile.js
+++ void/src/Components/Profile/Profile.js
@@ -30,7 +30,7 @@ class Profile extends Component {
 
   componentDidMount() {
     this.setState({
-      loggedInUser : true
+      loggedInUser: true
     })
     this.props.changeTitle('Login')
   }
@@ -59,11 +59,10 @@ class Profile extends Component {
   });
  };
 
- async deleteAccount() {
-  const deletedUser = await axios.delete(`/api/delete_account`);
-  
-  this.setState({
-    user: deletedUser.data
+ deleteAccount() {
+  axios.delete(`/api/delete_account`).then(res => {
+    this.props.setUser(res.data[0]);
+    window.alert('Thanks for using Void!')
   });
  };
 
@@ -82,8 +81,15 @@ class Profile extends Component {
         </div>
         <div className='profile-display'>
           {editImage 
-            ? <div className='picture-holder'> <input onChange={(e) => {this.toggle('img', e.target.value)}} placeholder='New Image' />
-              <button onClick={() => {img ? this.updatePic() : (window.alert('Please input a new image.')); this.toggle('editImage', false);}} >Save</button> </div>
+            ? <div className='picture-holder'> 
+                <div className='edit-holder'>
+                  <input onChange={(e) => {this.toggle('img', e.target.value)}} placeholder='New Image' />
+                  <span>
+                    <button onClick={() => this.toggle('editImage', false)}>Cancel</button>
+                    <button onClick={() => {img ? this.updatePic() : (window.alert('Please input a new image.')); this.toggle('editImage', false);}} >Save</button> 
+                  </span>
+                </div>
+              </div>
               : <div className='picture-holder'> 
                   <img src={this.props.user.image} className='user-image' onClick={() => this.toggle('editImage', true)} alt={`${this.props.user.username}'s profile pic`} /> 
                     <div className='middle'>
@@ -93,24 +99,43 @@ class Profile extends Component {
           }
           <div className='info-holder'>
           {editName 
-            ? <div className='username-holder'> <input onChange={(e) => {this.toggle('username', e.target.value)}} placeholder='New Username' />
-              <button onClick={() => {username ? this.updateUsername() : (window.alert('Please enter a new username')); this.toggle('editName', false);}} >Save</button> </div>
-            : <div className='username-holder'> <label className='username'>Username: {this.props.user.username}</label>
-              <button onClick={() => this.toggle('editName', true)}>Edit</button> </div>
+            ? <div className='username-holder'> 
+                <input onChange={(e) => {this.toggle('username', e.target.value)}} placeholder='New Username' />
+                <span>
+                  <button onClick={() => this.toggle('editName', false)}>Cancel</button>
+                  <button onClick={() => {username ? this.updateUsername() : (window.alert('Please enter a new username.')); this.toggle('editName', false);}} >Save</button> 
+                </span>
+              </div>
+            : <div className='username-holder'> 
+                <label className='username'>Username: {this.props.user.username}</label>
+                <button onClick={() => this.toggle('editName', true)}>Edit</button> 
+              </div>
           }
           {editEmail
-            ? <div className='email-holder'> <input onChange={(e) => {this.toggle('email', e.target.value)}} placeholder='New Email' />
-              <button onClick={() => {email ? this.updateEmail() : (window.alert('Please enter a new email')); this.toggle('editEmail', false);}} >Save</button> </div>
+            ? <div className='email-holder'> 
+                <input onChange={(e) => {this.toggle('email', e.target.value)}} placeholder='New Email' />
+                <span>
+                  <button onClick={() => this.toggle('editEmail', false)}>Cancel</button>
+                  <button onClick={() => {email ? this.updateEmail() : (window.alert('Please enter a new email.')); this.toggle('editEmail', false);}} >Save</button> 
+                </span>
+              </div>
             : <div className='email-holder'> <label className='email'>Email: {this.props.user.email}</label>
-              <button onClick={() => this.toggle('editEmail', true)}>Edit</button> </div>
+                <button onClick={() => this.toggle('editEmail', true)}>Edit</button> 
+              </div>
           }
           {editPassword
-            ? <div className='password-holder'> 
-              <input type='password' onChange={(e) => {this.toggle('oldPassword', e.target.value)}} placeholder='Current Password' />
-              <input type='password' onChange={(e) => {this.toggle('password', e.target.value)}} placeholder='New Password' />
-              <button onClick={() => {password ? this.updatePassword() : (window.alert('Please enter a new password')); this.toggle('editName', false);}} >Save</button> </div>
-            : <div className='password-holder'> <label className='password'>Change Password</label>
-              <button onClick={() => this.toggle('editPassword', true)}>Edit</button> </div>
+            ? <div className='password-holder-edit'> 
+                <input type='password' onChange={(e) => {this.toggle('oldPassword', e.target.value)}} placeholder='Current Password' />
+                <input type='password' onChange={(e) => {this.toggle('password', e.target.value)}} placeholder='New Password' />
+                <span>
+                  <button onClick={() => this.toggle('editPassword', false)}>Cancel</button>
+                  <button onClick={() => {password ? this.updatePassword() : (window.alert('Please enter a new password.')); this.toggle('editName', false);}} >Save</button>
+                </span>
+              </div>
+            : <div className='password-holder'> 
+                <label className='password'>Change Password</label>
+                <button onClick={() => this.toggle('editPassword', true)}>Edit</button> 
+              </div>
           }
           <button className='delete-account-btn' onClick={() => {this.deleteAccount(); this.props.setUser(null)}}>Delete Account</button>
           </div>

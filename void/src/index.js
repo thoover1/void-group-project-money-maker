62a92d8710337054a55dcf4ae1dc31524f2dfd4e
thoover1 <thoover1@uab.edu>
2020-01-15 20:19:11 -0600

    app mod

--- void/src/index.js
+++ void/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter} from 'react-router-dom';
-import {Provider} from 'react-redux';
+cdimport {Provider} from 'react-redux';
 import store from './store';
 import './index.css';
 import App from './App';
